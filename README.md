97186242f1d12a900e44e8f4ca8f44086d249884
Alex Kir <alexkir800@gmail.com>
2023-03-20 11:16:48 +0200

    add README.md and test

---
+++ README.md
@@ -0,0 +1,7 @@
+# Домашнее задание к занятию «2.4. BDD»
+
+## Задача №1: Page Object's
+
+Project status
+
+[![Build status](https://ci.appveyor.com/api/projects/status/m50p9pgnqqbv3502/branch/main?svg=true)](https://ci.appveyor.com/project/Akir800/bdd-page-object/branch/main)
--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -3,7 +3,48 @@ package ru.netology.web.data;
 import lombok.Value;
 
 public class DataHelper {
-  private DataHelper() {}
+  private DataHelper() {
+  }
+
+  public static AuthInfo getAuthInfo() {
+    return new AuthInfo("vasya", "qwerty123");
+  }
+
+  public static CardId getFirstCardId() {
+    return new CardId("92df3f1c-a033-48e6-8390-206f6b1f56c0");
+  }
+
+  public static CardId getSecondCardId() {
+    return new CardId("0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+  }
+
+  public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+    return new VerificationCode("12345");
+  }
+
+  public static TransferInfo getFirstCardTransferInfoPositive() {
+    return new TransferInfo(6_000, "5559 0000 0000 0002");
+  }
+
+  public static TransferInfo getSecondCardTransferInfoPositive() {
+    return new TransferInfo(5_100, "5559 0000 0000 0001");
+  }
+
+  public static TransferInfo getFirstCardTransferInfoNegative() {
+    return new TransferInfo(-1000, "5559 0000 0000 0002");
+  }
+
+  public static TransferInfo getSecondCardTransferInfoNegative() {
+    return new TransferInfo(21_000, "5559 0000 0000 0001");
+  }
+
+  public static TransferInfo setFirstCardTransferInfo(int amountTransfer) {
+    return new TransferInfo(amountTransfer, "5559 0000 0000 0002");
+  }
+
+  public static TransferInfo setSecondCardTransferInfo(int amountTransfer) {
+    return new TransferInfo(amountTransfer, "5559 0000 0000 0001");
+  }
 
   @Value
   public static class AuthInfo {
@@ -11,20 +52,19 @@ public class DataHelper {
     private String password;
   }
 
-  public static AuthInfo getAuthInfo() {
-    return new AuthInfo("vasya", "qwerty123");
-  }
-
-  public static AuthInfo getOtherAuthInfo(AuthInfo original) {
-    return new AuthInfo("petya", "123qwerty");
-  }
-
   @Value
   public static class VerificationCode {
     private String code;
   }
 
-  public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
-    return new VerificationCode("12345");
+  @Value
+  public static class CardId {
+    private String id;
+  }
+
+  @Value
+  public static class TransferInfo {
+    private int amount;
+    private String numberCard;
   }
 }
